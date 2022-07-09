8fb0feecd6fa8003c569b49e09827202f9535ad5
AlejandroHDZ2021 <alexhernandez.pro98@gmail.com>
2022-07-09 08:35:15 -0500

    shemas

--- db/mongodb.js
+++ db/mongodb.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const url = 'mongodb+srv://alejandro:KjdGXn2BdSC5ccW5@cluster0.n6uqn.mongodb.net/?retryWrites=true&w=majority'
+const url = 'mongodb+srv://alejandro:KjdGXn2BdSC5ccW5@cluster0.n6uqn.mongodb.net/store?retryWrites=true&w=majority'
 
 mongoose.connect(url, {}, () => {
     console.log('----------------')
